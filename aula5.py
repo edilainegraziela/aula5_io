1245a78af05831d46c1546c489780a1ad4251ae1
edilainegraziela <edilainegraziela@hotmail.com>
2024-11-23 17:01:37 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,54 @@
+
+# # por padrão o input é um texto
+# numero1 = input ('Digite um número: ')
+# numero2 = input ('Digite um número: ')
+# soma = numero1 + numero2
+# print(soma)
+
+# nome = input('Digite seu nome:')
+# print('Seja bem vindo(a)' ,nome)
+# print('SEJA BEM VINDO(A)'+ ' '+nome)
+# print(f'SEJA BEM VINDO(A) {nome}')
+# print('SEJA BEM VINDO(A) {} ' .format(nome))
+# print('SEJA BEM VINDO(A) %s '% (nome))
+
+# texto = 'olá mundo!'
+# print(len(texto))
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+print('Seja Bem Vindo (a)')
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+verdadeiro = True
+falso = False
+tipo3 = type(verdadeiro)
+print(tipo3)
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+print(12.5*5.3)
+# 4 Imprima o resultado da divisão de dois números inteiros de sua escolha.
+print(100/5)
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+print(50-20)
+# 6 Imprima o resultado da divisão inteira de dois números inteiros de sua escolha.
+print(60/3)
+# 7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+print(12.5*5.3*1.2*8.7)
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número.
+numero = 10
+print(numero*2)
+# 9 Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel)
+numero1=input ('Digite um Número:')
+numero2=input ('Digite outro Número para somar:')
+soma=numero1+numero2
+print(soma)
+# 10 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+nome = input('Nome:')
+nascimento = input('Data de Nascimento:')
+cpf = input('CPF:')
+telefone= input('Telefone:')
+print('..........................................')
+print('DADOS CADASTRADOS:')
+
+
+
+
+
